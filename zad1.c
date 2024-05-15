d3255fd8ea0817af301383f3eb33e95d6124523a
imgres <92025754+imgres2002@users.noreply.github.com>
2024-05-15 13:00:32 +0200

    Update zad1.c

--- zad1.c
+++ zad1.c
@@ -1,18 +1,45 @@
-int check_button(unsigned zad){
-    prev6 = PORTDbits.RD6;      //scanning for a change of buttons' state
-    prev7 = PORTDbits.RD7;
-    __delay32(150000);
+// CONFIG2
+#pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode selected)
+#pragma config OSCIOFNC = OFF           // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRIPLL           // Oscillator Select (Primary Oscillator with PLL module (HSPLL, ECPLL))
+#pragma config IESO = OFF               // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog Timer is disabled)
+#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+
+
+#include <xc.h>
+#include <libpic30.h>
+#include <stdio.h>
+#include <math.h>
+
+
+
+unsigned int seed = 1110011;
+char current6 = 0, current7 = 0; //variables for buttons
+int zad = 1;
+
+int check_button(int zad){
     current6 = PORTDbits.RD6;
     current7 = PORTDbits.RD7;
+    __delay32(1500000);
 
-    if (current6 - prev6 == 1){
+    if (current6 == 1){
         zad++;
         if(zad > 9){
-            zad=0;
+            zad=1;
         }
     }
 
-    if (current7 - prev7 == 1){
+    if (current7 == 1){
         zad--;
         if(zad < 1){
             zad=9;
@@ -26,60 +53,62 @@ int IntToGray(unsigned char input){
 }
 
 unsigned int generate_random() {
-    seed ^= (seed << 13);
-    seed ^= (seed >> 17);
-    seed ^= (seed << 5);
-    return seed & 0x3F;
+    
 }
 
 int main(void) {
-    unsigned int seed = 1110011;
-    unsigned portValue = 0x0000;
-    char current6 = 0, prev6 = 0, current7 = 0, prev7 = 0; //variables for buttons
-    unsigned zad = 1;
-
     // Port access
     TRISA = 0x0000;     // port set to output
     TRISD = 0xFFFF;     // port set to input
+    
+    unsigned portValue = 0x0000;
+
+    
 
     while(1)
     {
         LATA = portValue;
-        prev6 = PORTDbits.RD6;      //scanning for a change of buttons' state
-        prev7 = PORTDbits.RD7;
-        __delay32(150000);
-        current6 = PORTDbits.RD6;
-        current7 = PORTDbits.RD7;
 
         while(zad == 1) {
             portValue++;
             LATA = portValue;
             __delay32(1500000);
             zad = check_button(zad);
+   
         }
+        portValue = 0x0000;
 
         while (zad == 2) {
-            portValue++;
+            portValue--;
             LATA = portValue;
             __delay32(1500000);
             zad = check_button(zad);
         }
+        portValue = 0x0000;
 
         while (zad == 3){
             for (int i = 0; i <= 255; i++) {
                 LATA = IntToGray(i);
                 __delay32(1500000);
                 zad = check_button(zad);
+                if (zad != 3){
+                    break;
+                }
             }
         }
+        portValue = 0x0000;
 
         while (zad == 4){
             for (int i = 255; i >= 0; i--) {
                 LATA = IntToGray(i);
                 __delay32(1500000);
                 zad = check_button(zad);
+                if (zad != 4){
+                    break;
+                }
             }
         }
+        portValue = 0x0000;
 
         while (zad == 5){
             int dziesietna = 0;
@@ -89,21 +118,29 @@ int main(void) {
                     LATA = dziesietna + j;
                     __delay32(1500000);
                     zad = check_button(zad);
+                    if (zad != 5){
+                        break;
+                    }
                 }
             }
         }
+        portValue = 0x0000;
 
         while (zad == 6){
             int dziesietna = 0;
-            for (int i = 0; i >= 0; i--) {
+            for (int i = 9; i >= 0; i--) {
                 dziesietna = 16 * i;
                 for (int j = 9; j >= 0; j--) {
                     LATA = dziesietna + j;
                     __delay32(1500000);
                     zad = check_button(zad);
+                    if (zad != 6){
+                        break;
+                    }
                 }
             }
         }
+        portValue = 0x0000;
 
         while (zad == 7) {
             int wezyk = 0;
@@ -111,7 +148,11 @@ int main(void) {
             while (zad==7) {
                 for (int i = wezyk; i < wezyk + 3; i++) {
                     portValue += pow(2, i);
+                    __delay32(1500000);
                     zad = check_button(zad);
+                    if (zad != 7){
+                        break;
+                    }
                 }
                 if (wezyk + 4 > 8) {
                     strona = 0;
@@ -125,11 +166,12 @@ int main(void) {
                 if (strona == 0) {
                     wezyk--;
                 }
-                portValue = 0;
                 LATA = portValue;
+                portValue = 0;
                 __delay32(1500000);
             }
         }
+        portValue = 0x0000;
 
         while (zad==8){
             unsigned kolejka = 0;
@@ -140,71 +182,24 @@ int main(void) {
                     LATA = portValue;
                     __delay32(1500000);
                     zad = check_button(zad);
+                    if (zad != 8){
+                        break;
+                    }
                 }
                 kolejka += pow(2, j);
                 j--;
             }
             portValue=0;
         }
+        portValue = 0x0000;
 
         while (zad==9){
-            portValue = generate_random();
-            LATA = portValue;
+            LATA = 0x0000;
             __delay32(1500000);
+            LATA = 0xffff;
             zad = check_button(zad);
         }
-
-        portValue=0;
+        portValue = 0x0000;
     }
     return 0;
 }
-
-//int main(void) {
-//    unsigned portValue = 0x0001;
-//    char current6 = 0, prev6 = 0, current7 = 0, prev7 = 0; //variables for buttons
-//    int zad = 0;
-//
-//    // Port access
-//    TRISA = 0x0000;     // port set to output
-//    TRISD = 0xFFFF;     // port set to input
-//    
-//    while(1)
-//    {
-//        
-//        LATA = portValue;
-//        prev6 = PORTDbits.RD6;      //scanning for a change of buttons' state
-//        prev7 = PORTDbits.RD7;
-//        __delay32(150000);
-//        current6 = PORTDbits.RD6;
-//        current7 = PORTDbits.RD7;
-//        if (current6 - prev6 == 1) //button up
-//        {   
-//            zad++;
-//            if(zad > 9){
-//                zad=0;
-//            }
-//            
-//        }
-//        if (current6 - prev6 == 1) //button up
-//        {
-//            zad--;
-//            if(zad < 0){
-//                zad=9;
-//            }
-//        }
-//        if (zad == 1) {
-//            while(1){
-//                portValue++;
-//                LATA = portValue;
-//            }
-//        }
-//        
-//        if (zad == 2) {
-//            while(current6==prev6 && current7 == prev7){
-//                portValue--;
-//            }
-//        }
-//    }
-//
-//    return 0;
-//}
