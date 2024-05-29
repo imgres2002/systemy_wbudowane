cac54073e1a50dcc0c88268814cd24166715162b
imgres <92025754+imgres2002@users.noreply.github.com>
2024-05-29 13:49:42 +0200

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -121,25 +121,22 @@ void check_button(int *opcja, int *start){
 int check_potentiometer(int opcja){
     TRISA = 0x0000;      //TRISA = 0b0000000000000000;
     TRISB = 0xFFFF;      //TRISB = 0b1111111111111111;
-    __delay_ms(100);
     AD1CON1 = 0x80E4;   //AD1CON1 = 0b1000000011100100;
     AD1CON2 = 0x0404;   //AD1CON2 = 0b0000010000000100;
     AD1CON3 = 0x0F00;   //AD1CON3 = 0b0000111100000000;
     AD1CHS = 0;
     AD1CSSL = 0x0020;   //AD1CSSL = 0b0000000000100000;
-    __delay_ms(100);
-    float val = 0;
-    __delay_ms(100);
-    potentiometer = ADC1BUF0;
+    int val = 0;
     __delay_ms(100);
     if(opcja == 0){
-        val = potentiometer/1024 * 300;
+//        float pom = ADC1BUF0/ 1024 * 300;
+//        val = (int)pom;
+        val = ADC1BUF0;
     }
     __delay_ms(100);
     if(opcja == 1){
-        val = potentiometer/1024 * 600;
+        val = ADC1BUF0/2;
     }
-    __delay_ms(100);
     TRISB = 0x7FFF;
     TRISD = 0x0000;
     TRISE = 0x0000;
@@ -174,6 +171,8 @@ int main(void) {
             sprintf(tekst, "moc: %dw", waty);
             LCD_setCursor(2, 0);
             __delay_ms(100);
+            LCD_sendCommand(LCD_CLEAR);
+            __delay_ms(100);
             LCD_print(tekst);
             __delay_ms(100);
         }
@@ -193,7 +192,7 @@ int main(void) {
             while (start == 1 && czas > 0) {
                 LCD_setCursor(1, 0);
                 __delay_ms(100);
-                sprintf(tekst, "pozosto: %02d:%02ds", czas / 60, czas % 60);
+                sprintf(tekst, "pozostolo: %02d:%02ds", czas / 60, czas % 60);
                 LCD_print(tekst);
                 __delay_ms(9800);
                 czas--;
